1f9a818f23eba005c0c045b17242bc0917caa6cf
Henrique Bonatto <nattohen@gmail.com>
2023-09-20 11:08:38 -0300

    Initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/login.css">
+    <link rel="shortcut icon" href="/img/logo.ico" type="image/x-icon">
+    <title>RouteLog - Login</title>
+</head>
+<body>
+    <main id="conteudo">
+        <div class="esquerda">
+            <img src="/img/logo.svg" alt="">
+        </div>
+        <div class="direita">
+            <h1 class="titulo">LOGIN ADMIN</h1>
+
+            <form action="" method="post">
+                <label for="usuario">Usuario</label>
+                <input type="text" name="usuario" id="usuario">
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" id="senha">
+
+                <button type="submit">Efetuar login</button>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
